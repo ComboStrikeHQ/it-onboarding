c8defeec73e85ec314290435d1b7554f46becd4a
soendagsbillist <vladislav.tyutin@me.com>
2020-05-12 16:32:24 +0200

    Add create_user script
    It creates standard user and its home directory

---
+++ create_user.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USERNAME="cstest"
+FULLNAME="John Doe"
+PASSWORD="combostrike"
+
+# ====
+
+if [[ $UID -ne 0 ]]; then echo "Please run $0 as root." && exit 1; fi
+
+# Find out the next available user ID
+MAXID=$(dscl . -list /Users UniqueID | awk '{print $2}' | sort -ug | tail -1)
+USERID=$((MAXID+1))
+
+# Create the user account
+dscl . -create /Users/$USERNAME
+dscl . -create /Users/$USERNAME UserShell /bin/bash
+dscl . -create /Users/$USERNAME RealName "$FULLNAME"
+dscl . -create /Users/$USERNAME UniqueID "$USERID"
+dscl . -create /Users/$USERNAME PrimaryGroupID 20
+dscl . -create /Users/$USERNAME NFSHomeDirectory /Users/$USERNAME
+
+dscl . -passwd /Users/$USERNAME $PASSWORD
+
+
+# Create the home directory
+createhomedir -c -u $USERNAME > /dev/null
+
+echo "Created user #$USERID: $USERNAME ($FULLNAME)"
