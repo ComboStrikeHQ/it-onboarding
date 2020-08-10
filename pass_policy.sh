a27ef6f2c3d3f2a7efd851decbd0e19dd8e34f90
soendagsbillist <vladislav.tyutin@me.com>
2020-08-10 17:32:55 +0200

    Dropped pass policy into 2 files to avoid using root on set-up.sh
    Homebrew installation script prohibits using root, so workaround was
    needed in order to run both of these things in one installation
    chain.

---
+++ pass_policy.sh
@@ -0,0 +1,28 @@
+# Set password policies 10 chars min length and prompt for a new password after reboot
+MIN_LENGTH=10
+
+LOGGEDINUSER=$(ls -l /dev/console | awk '{print $3}')
+
+
+echo "<dict>
+ <key>policyCategoryPasswordContent</key>
+   <array>
+    <dict>
+     <key>policyContent</key>
+      <string>policyAttributePassword matches '.{$MIN_LENGTH,}+'</string>
+     <key>policyIdentifier</key>
+      <string>Has at least $MIN_LENGTH characters</string>
+     <key>policyParameters</key>
+     <dict>
+      <key>minimumLength</key>
+       <integer>$MIN_LENGTH</integer>
+     </dict>
+    </dict>
+   </array>
+</dict>" > /private/var/tmp/pwpolicy.plist
+
+
+sudo pwpolicy -u $LOGGEDINUSER -clearaccountpolicies
+sudo pwpolicy -u $LOGGEDINUSER -setaccountpolicies /private/var/tmp/pwpolicy.plist
+
+sudo pwpolicy -a $LOGGEDINUSER -u $LOGGEDINUSER -setpolicy "newPasswordRequired=1"
--- set-up.sh
+++ set-up.sh
@@ -53,34 +53,8 @@ sudo /usr/bin/defaults write /Library/Preferences/com.apple.commerce.plist AutoU
 /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsignedapp off
 
 # Restart firewall
-pkill -HUP socketfilterfw
+sudo pkill -HUP socketfilterfw
 
+# Set up password policy
+sudo /bin/bash ./pass-policy.sh
 
-# Set password policies 10 chars min length and prompt for a new password after reboot
-MIN_LENGTH=10
-
-LOGGEDINUSER=$(ls -l /dev/console | awk '{print $3}')
-
-
-echo "<dict>
- <key>policyCategoryPasswordContent</key>
-   <array>
-    <dict>
-     <key>policyContent</key>
-      <string>policyAttributePassword matches '.{$MIN_LENGTH,}+'</string>
-     <key>policyIdentifier</key>
-      <string>Has at least $MIN_LENGTH characters</string>
-     <key>policyParameters</key>
-     <dict>
-      <key>minimumLength</key>
-       <integer>$MIN_LENGTH</integer>
-     </dict>
-    </dict>
-   </array>
-</dict>" > /private/var/tmp/pwpolicy.plist
-
-
-sudo pwpolicy -u $LOGGEDINUSER -clearaccountpolicies
-sudo pwpolicy -u $LOGGEDINUSER -setaccountpolicies /private/var/tmp/pwpolicy.plist
-
-sudo pwpolicy -a $LOGGEDINUSER -u $LOGGEDINUSER -setpolicy "newPasswordRequired=1"
